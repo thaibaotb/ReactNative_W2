d86177b0627b7d5e73a3d7ec1f305de9fa62eadd
thaibaotb <dangthaibao14@gmail.com>
2025-09-17 00:00:48 +0700

    Câu 15

---
+++ src/bai15.ts
@@ -0,0 +1,11 @@
+// async function tripleAfter1Sec(n: number): Promise<number> {
+//     await new Promise(r => setTimeout(r, 1000));
+//     return n * 3;
+//   }
+// async function seq() {
+//     const a = await tripleAfter1Sec(2);
+//     const b = await tripleAfter1Sec(3);
+//     console.log(a, b);
+//   }
+//   seq();
+  
