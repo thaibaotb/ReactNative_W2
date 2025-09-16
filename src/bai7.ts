8f669fd7f680c954e0cd8924c2837ac8f145775d
thaibaotb <dangthaibao14@gmail.com>
2025-09-16 23:59:35 +0700

    Câu 7

---
+++ src/bai7.ts
@@ -0,0 +1,10 @@
+// function simulateTask(time: number): Promise<string> {
+//     return new Promise(resolve => {
+//       setTimeout(() => resolve("Task done"), time);
+//     });
+//   }
+// Promise.race([
+//     simulateTask(1000),
+//     simulateTask(2000)
+//   ]).then(result => console.log("First done:", result));
+  
