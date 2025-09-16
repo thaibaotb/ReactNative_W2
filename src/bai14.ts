2a1236e021a15c2a833cb08df698cc429269d40a
thaibaotb <dangthaibao14@gmail.com>
2025-09-17 00:00:41 +0700

    Câu 14

---
+++ src/bai14.ts
@@ -0,0 +1,6 @@
+// async function tripleAfter1Sec(n: number): Promise<number> {
+//     await new Promise(r => setTimeout(r, 1000));
+//     return n * 3;
+//   }
+//   tripleAfter1Sec(5).then(console.log);
+// //   
