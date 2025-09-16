b35a9a8b87b262d6af26eba4619f3156a1ffbe69
thaibaotb <dangthaibao14@gmail.com>
2025-09-16 23:59:29 +0700

    Câu 6

---
+++ src/bai6.ts
@@ -0,0 +1,11 @@
+// function simulateTask(time: number): Promise<string> {
+//     return new Promise(resolve => {
+//       setTimeout(() => resolve("Task done"), time);
+//     });
+//   }
+// Promise.all([
+//     simulateTask(1000),
+//     simulateTask(2000),
+//     simulateTask(1500)
+//   ]).then(result => console.log("All done:", result));
+  
