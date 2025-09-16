20668b07008abc74810c53b7d68d1a6a409f8250
thaibaotb <dangthaibao14@gmail.com>
2025-09-17 00:01:17 +0700

    Câu 18

---
+++ src/bai18.ts
@@ -0,0 +1,6 @@
+// async function fetchUser(id: number): Promise<{id:number,name:string}> {
+//     await new Promise(r => setTimeout(r, 1000));
+//     return { id, name: `User${id}` };
+//   }
+//   fetchUser(1).then(console.log);
+  
